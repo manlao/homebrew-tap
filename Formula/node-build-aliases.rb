91ebd108a77ff75cd4ef57faff2fd1cd0b004b6a
Sean Tu <manlao@gmail.com>
2021-04-23 11:02:07 +0800

    node-build-aliases 1.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/node-build-aliases.rb
+++ Formula/node-build-aliases.rb
@@ -1,8 +1,8 @@
 class NodeBuildAliases < Formula
   desc "Create aliases for node-build definitions"
   homepage "https://github.com/manlao/node-build-aliases"
-  url "https://github.com/manlao/node-build-aliases/archive/1.1.0.tar.gz"
-  sha256 "aed42e1e170e8b2e755042cfa72f3fb7b3f76870c06221e0ad63e8c16433f0e3"
+  url "https://github.com/manlao/node-build-aliases/archive/1.1.1.tar.gz"
+  sha256 "e77c551c5892a3a8d62cfb6cf814532e67694bd746b84cb75b2758b26f3fb8bb"
   head "https://github.com/manlao/node-build-aliases.git"
 
   depends_on "nodenv"
