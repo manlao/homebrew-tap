ada118ea87e11520ac4642bdda04cdc0171bd37d
Sean Tu <manlao@gmail.com>
2021-04-23 15:26:15 +0800

    fix: fix depends_on order issue

--- Formula/node-build-aliases.rb
+++ Formula/node-build-aliases.rb
@@ -5,8 +5,8 @@ class NodeBuildAliases < Formula
   sha256 "12c0ea7c7cbf53f31eb1a19a638bf978c64a0b93d4fb9d59c7cfd5b541009e2d"
   head "https://github.com/manlao/node-build-aliases.git"
 
-  depends_on "nodenv"
   depends_on "node-build"
+  depends_on "nodenv"
   depends_on "nodenv/nodenv/node-build-update-defs"
 
   def install
--- Formula/nodenv-auto-install.rb
+++ Formula/nodenv-auto-install.rb
@@ -5,8 +5,8 @@ class NodenvAutoInstall < Formula
   sha256 "28f0aa277bfbf241f3c1f87559ffd8c4468612dfcd7442e9b35316c64009c50b"
   head "https://github.com/manlao/nodenv-auto-install.git"
 
-  depends_on "nodenv"
   depends_on "node-build"
+  depends_on "nodenv"
 
   def install
     prefix.install Dir["*"]
